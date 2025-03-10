1673b022cfea0806901ae9ac882f376c3b996414
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2025-03-10 08:09:31 +0100

    Added ensure_installed to mason

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,23 +1,39 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function ()
-      require('mason').setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function ()
-      require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls"}
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function ()
-      -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      require("plugins.config.lspconfig")
-    end
-  }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup {
+				ensure_installed = {
+					"lua_ls",
+					"html",
+					"cssls",
+					"ts_ls",
+					"svelte",
+					"tailwindcss",
+					"eslint",
+					"sqlls",
+					"gopls",
+					"pylsp",
+					"bashls",
+					"prismals",
+					"dockerls",
+					"docker_compose_language_service",
+					"rust_analyzer",
+				},
+			}
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			-- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			require "plugins.config.lspconfig"
+		end,
+	},
 }
